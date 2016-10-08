8d97949e4b3c09b2f1078660313043a776bb6c93
pcrisostomo <crisostomopau@gmail.com>
2016-10-08 14:17:27 +0800

    fixed bugs

--- app/src/main/java/com/mlabs/bbm/iquiz/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/iquiz/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     Intent intent = new Intent(MainActivity.this, On_Touch.class);
                     startActivity(intent);
+                    finish();
 
                 }
             });
